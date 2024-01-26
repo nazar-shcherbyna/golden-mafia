a2950e0ef0824db4c05e939fed4e354abbfa6f23
Nazar Shcherbyna <shherbina.nazar@gmail.com>
2024-01-26 11:31:10 +0100

    add game layout and loading pages

---
+++ src/app/game/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <h1>Game routes layour</h1>
+      {children}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <div>Main Page</div>;
+export default async function Home() {
+  // const users = await new Promise<
+  //   Array<{
+  //     id: number;
+  //     name: string;
+  //   }>
+  // >((resolve) => {
+  //   setTimeout(() => {
+  //     resolve([
+  //       { id: 1, name: "John" },
+  //       { id: 2, name: "Jane" },
+  //     ]);
+  //   }, 4000);
+  // });
+
+  // console.log("users", users);
+
+  return (
+    <div className="w-full h-full flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500">
+      <h1 className="text-white text-6xl font-semibold">🔐 Auth</h1>
+      {/* <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul> */}
+    </div>
+  );
 }
