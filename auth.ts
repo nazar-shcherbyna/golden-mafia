fcf3fb9917cfa332ff695142fae87f9bf5242595
Nazar Shcherbyna <shherbina.nazar@gmail.com>
2024-01-26 11:29:45 +0100

    update GET, POST reexports

--- auth.ts
+++ auth.ts
@@ -1,6 +1,11 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
   ...authConfig,
 });
